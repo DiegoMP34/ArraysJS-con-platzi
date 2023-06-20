910619c27f0269ca94b53482a2145d4d9c0b623f
DiegoMP34 <diegomarcosportugal7@gmail.com>
2023-06-19 21:10:24 -0500

    Repaso del la diferencia entre forEach y map

--- server/01-for-each.js
+++ server/01-for-each.js
@@ -6,3 +6,29 @@ for (let letter = 0; letter < letters.length; letter++) {
 }
 
 letters.forEach(letter => console.log('foreach', letter))
+
+// vemos como despues del forEach el numbers sigue intacto
+let numbers = [1, 2, 3, 4, 5]
+console.log(numbers)
+
+numbers.forEach(number => console.log( number * 10))
+
+console.log(numbers)
+
+// aqui si afecta al array number.
+numbers.forEach((number, _index, numbers) => {
+    numbers.push(number * 2)
+})
+
+console.log(numbers)
+
+// pero si usamos map
+let numbers1 = [1,  2, 3, 4, 5]
+
+console.log(numbers1)
+
+//nos devolveria un array distinto sin afectar al principal.
+const newNumbers1 = numbers1.map(number => number * 2)
+
+console.log(numbers1)
+console.log(newNumbers1)
